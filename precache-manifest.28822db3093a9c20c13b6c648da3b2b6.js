db572d29f8b18fcb67eff61694ddcbb2c4246223
Amr Elnaggar <amr.eln2gar@gmail.com>
2020-07-22 16:35:35 +0200

    Updates

---
+++ precache-manifest.28822db3093a9c20c13b6c648da3b2b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb29c45b7d9afed344ba39284cc9b1f5",
+    "url": "/AmrElnaggar99/reactnd-project-myreads-starter/index.html"
+  },
+  {
+    "revision": "2ddf74eefb46cf670a03",
+    "url": "/AmrElnaggar99/reactnd-project-myreads-starter/static/css/main.68e45077.chunk.css"
+  },
+  {
+    "revision": "00e2289fe80b91976c78",
+    "url": "/AmrElnaggar99/reactnd-project-myreads-starter/static/js/2.5ece63d8.chunk.js"
+  },
+  {
+    "revision": "38dadbf4a04fc896e329778ea7e57a04",
+    "url": "/AmrElnaggar99/reactnd-project-myreads-starter/static/js/2.5ece63d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddf74eefb46cf670a03",
+    "url": "/AmrElnaggar99/reactnd-project-myreads-starter/static/js/main.0b77e382.chunk.js"
+  },
+  {
+    "revision": "1f1ca2d88c7a23f28638",
+    "url": "/AmrElnaggar99/reactnd-project-myreads-starter/static/js/runtime-main.e452d509.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/AmrElnaggar99/reactnd-project-myreads-starter/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/AmrElnaggar99/reactnd-project-myreads-starter/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/AmrElnaggar99/reactnd-project-myreads-starter/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
